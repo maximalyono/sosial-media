bd03bad7312f796dc8a1f169e9961975051a8cd4
Deni MMalyono <maximalyono@gmail.com>
2019-11-29 21:22:47 +0700

    [malyono] mengedit index, we wellcome you

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-8">
-                        <h1>1 Welcome, Selamat datang di Pesbuker</h1>
+                        <h1>We Wellcome you, Selamat datang di Pesbuker</h1>
                         <p>Bergabunglah bersama bermiliaraan orang lainnya...</p>
                     </div>
                     <div class="col-md-4">
